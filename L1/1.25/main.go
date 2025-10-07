8869baf1ea312a03c5837ae32059cbdffa740bbf
alex <shimaxx@mail.ru>
2025-10-07 23:39:35 +0300

    1.25 sleep with ctx done

---
+++ L1/1.25/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func sleep(duration time.Duration) {
+	start := time.Now()
+	deadline := start.Add(duration)
+	ctx, cancel := context.WithDeadline(context.Background(), deadline)
+	defer cancel()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func main() {
+	sleep(5 * time.Second)
+	fmt.Println("ive waited for 5 seconds")
+}
